4205837e21e234f112a592dcb441a8a5b2a60afe
Paul Petrov <paul@bargamut.ru>
2020-11-02 17:13:45 +0300

    Add test calls to actions to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,32 @@
 
 import initialState from './initialState';
 import { createStore } from 'redux';
+import createPost from './actionCreators/createPost';
 
 const store = createStore(reducer, initialState);
+
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => console.log(`Subscribe: ${store.getState()}`));
+
+// Will notify by console.log below and by subscribe
+store.dispatch(
+  createPost(
+    { id: 1, title: `Test post creation` }
+  )
+);
+
+console.log(store.getState());
+
+unsubscribe();
+
+// Will notify only by consloe.log below
+store.dispatch(
+  createPost(
+    { id: 2, title: `Test post creation 2` }
+  )
+);
+
+console.log(store.getState());
 
 export default store;
