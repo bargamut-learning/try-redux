222d139c339568b2e17dfdefa89e0d5a1a509481
Paul Petrov <paul@bargamut.ru>
2020-11-02 15:03:45 +0300

    Update initialState & connect to createStore

---
+++ src/store/initialState.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const initialState = {
+  posts: [],
+};
+
+export default initialState;
--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,9 @@
 
 // Global store of the app
 
+import initialState from './initialState';
 import { createStore } from 'redux';
 
-const store = createStore(reducer);
+const store = createStore(reducer, initialState);
 
 export default store;
