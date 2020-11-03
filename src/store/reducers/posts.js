507457e5fa39ef171baeb120a17558c65347e30d
Paul Petrov <paul@bargamut.ru>
2020-11-03 14:50:12 +0300

    Create UPDATE_POST reducer

--- src/store/reducers/posts.js
+++ src/store/reducers/posts.js
@@ -1,12 +1,17 @@
 'use strict';
 
-import { CREATE_POST } from '../actionTypes/posts';
+import { CREATE_POST, UPDATE_POST } from '../actionTypes/posts';
 
 function postsReducer(state = [], action) {
   switch (action.type) {
     case CREATE_POST: return state.concat(action.payload); // { id: 1, title: `Test post` };
 
-    // case UPDATE_POST: return ...
+    case UPDATE_POST:
+      return state.map(post => {
+        return (post.id === action.payload.id)
+          ? { ...post, ...action.payload }
+          : post;
+      });
 
     default: return state;
   }
