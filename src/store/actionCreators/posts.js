cfb36798f0a6efda746642bbf0c5658c25d72f80
Paul Petrov <paul@bargamut.ru>
2020-11-03 14:47:31 +0300

    Add updatePost action creator into renamed posts.js

---
+++ src/store/actionCreators/posts.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import { CREATE_POST, UPDATE_POST } from '../actionTypes/posts';
+
+export function createPost(payload) {
+  return {
+    type: CREATE_POST,
+    payload: {
+      ...payload,
+      id: Date.now(),
+    }
+  }
+}
+
+export function updatePost(id, payload) {
+  return {
+    type: UPDATE_POST,
+    payload: {
+      ...payload,
+      id,
+    },
+  };
+}
