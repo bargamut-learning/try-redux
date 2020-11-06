954d482d8d1e3f72f0e87f2defbcea798a5131e8
Paul Petrov <paul@bargamut.ru>
2020-11-06 17:39:45 +0300

    Create posts mock-data

---
+++ mocks.js
@@ -0,0 +1,13 @@
+'use strict';
+
+export const POSTS = [
+  { id: 0, title: `Test`, },
+  { id: 1, title: `Test 1`, },
+  { id: 2, title: `Test 2`, },
+  { id: 3, title: `Test 3`, },
+  { id: 4, title: `Test 4`, },
+];
+
+export default {
+  POSTS,
+};
