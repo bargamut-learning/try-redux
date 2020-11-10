6a2c895d73bc3d4b731270afd1bf86ec9dd2919e
Paul Petrov <paul@bargamut.ru>
2020-11-10 12:48:31 +0300

    Create reducers for fetch posts

--- src/store/reducers/postsReducer.js
+++ src/store/reducers/postsReducer.js
@@ -1,9 +1,11 @@
 'use strict';
 
-import { CREATE_POST, UPDATE_POST } from '../actionTypes/postsActionTypes';
+import { CREATE_POST, POSTS_FETCH_SUCCEEDED, UPDATE_POST } from '../actionTypes/postsActionTypes';
 
 function postsReducer(state = [], action) {
   switch (action.type) {
+    case POSTS_FETCH_SUCCEEDED: return state.concat(action.payload);
+
     case CREATE_POST: return state.concat(action.payload); // { id: 1, title: `Test post` };
 
     case UPDATE_POST:
