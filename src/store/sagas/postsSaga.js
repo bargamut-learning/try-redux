2be9e30b2c0a0e43a06ad0f7545f1e0b37400daa
Paul Petrov <paul@bargamut.ru>
2020-11-10 15:01:38 +0300

    Rename saga for fetch posts

--- src/store/sagas/postsSaga.js
+++ src/store/sagas/postsSaga.js
@@ -25,7 +25,7 @@ function* fetchPost() {
 
 // Call fetchPosts by every dispatch of "POSTS_FETCH_REQUESTED"
 // Allows fetching data of many posts in same time
-function* mySaga() {
+function* watchFetchPostsSaga() {
   yield takeEvery(POSTS_FETCH_REQUESTED, fetchPost);
 }
 
@@ -35,9 +35,9 @@ Don't allows fetching data of many posts in same time
 If you have new dispatch when previous is pending
 then pending is canceled & works only latest
 
-function* mySaga() {
+function* watchFetchPostsSaga() {
   yield takeLatest(`POST_FETCH_REQUESTED`, fetchPosts);
 }
 */
 
-export default mySaga;
+export default watchFetchPostsSaga;
