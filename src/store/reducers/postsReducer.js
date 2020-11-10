5bb1a501491837a8eb416ef0650e01b070cd4f52
Paul Petrov <paul@bargamut.ru>
2020-11-10 20:59:39 +0300

    Refactoring posts reducer

--- src/store/reducers/postsReducer.js
+++ src/store/reducers/postsReducer.js
@@ -1,14 +1,16 @@
 'use strict';
 
-import { CREATE_POST, POSTS_FETCH_SUCCEEDED, UPDATE_POST } from '../actionTypes/postsActionTypes';
+import * as postsActionTypes from '../actionTypes/postsActionTypes';
 
-function postsReducer(state = [], action) {
+import initialState from '../initialState';
+
+function postsReducer(state = initialState.posts, action) {
   switch (action.type) {
-    case POSTS_FETCH_SUCCEEDED: return state.concat(action.payload);
+    case postsActionTypes.POSTS_FETCH_SUCCEEDED: return state.concat(action.payload);
 
-    case CREATE_POST: return state.concat(action.payload); // { id: 1, title: `Test post` };
+    case postsActionTypes.CREATE_POST: return state.concat(action.payload); // { id: 1, title: `Test post` };
 
-    case UPDATE_POST:
+    case postsActionTypes.UPDATE_POST:
       return state.map(post => {
         return (post.id === action.payload.id)
           ? { ...post, ...action.payload }
