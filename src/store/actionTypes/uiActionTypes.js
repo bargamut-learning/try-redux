a39bd89fdf5f2771745a07b9b5df972332b13d88
Paul Petrov <paul@bargamut.ru>
2020-11-10 20:58:56 +0300

    Create UI actions types

---
+++ src/store/actionTypes/uiActionTypes.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export const START_ACTION = `START_ACTION`;
+export const STOP_ACTION = `STOP_ACTION`;
+export const REFRESH_START_ACTION = `REFRESH_START_ACTION`;
+export const REFRESH_STOP_ACTION = `REFRESH_STOP_ACTION`;
