f2135de7fd11201b36326b556e446e8d8b8f985e
Paul Petrov <paul@bargamut.ru>
2020-11-10 12:49:01 +0300

    Create Redux-Saga for posts

---
+++ src/store/sagas/postsSaga.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { POSTS_FETCH_REQUESTED } from '../actionTypes/postsActionTypes';
+import { fetchPostsFailed, fetchPostsSucceeded } from '../actionCreators/postsActionCreators';
+import API from '../../api/postsAPI';
+
+// Imitation async calls
+function delay(ms) {
+  return new Promise(res => setTimeout(res, ms));
+}
+
+function* fetchPost() {
+  yield delay(1000);
+
+  try {
+    const posts = yield call(API.fetchPosts);
+
+    // Dispatch actions
+    yield put(fetchPostsSucceeded(posts));
+  } catch (error) {
+    yield put(fetchPostsFailed({ message: error.message }));
+  }
+}
+
+// Call fetchPosts by every dispatch of "POSTS_FETCH_REQUESTED"
+// Allows fetching data of many posts in same time
+function* mySaga() {
+  yield takeEvery(POSTS_FETCH_REQUESTED, fetchPost);
+}
+
+/* or
+
+Don't allows fetching data of many posts in same time
+If you have new dispatch when previous is pending
+then pending is canceled & works only latest
+
+function* mySaga() {
+  yield takeLatest(`POST_FETCH_REQUESTED`, fetchPosts);
+}
+*/
+
+export default mySaga;
